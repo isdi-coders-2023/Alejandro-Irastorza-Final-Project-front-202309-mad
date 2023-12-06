f5e8eb0c19fed05ec959ecbfa8583aa98d8ca8fb
alejandroileal <aleal1202@outlook.com>
2023-12-06 20:09:38 +0100

    Add Main with router and provider

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import App from './components/app/app.js';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.js';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>
+);
