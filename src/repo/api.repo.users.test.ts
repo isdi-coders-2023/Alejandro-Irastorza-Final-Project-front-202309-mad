72c1985912af7db985098343b3bf5933faf17f1f
alejandroileal <aleal1202@outlook.com>
2023-12-06 13:49:54 +0100

    Add users repo with test

---
+++ src/repo/api.repo.users.test.ts
@@ -0,0 +1,52 @@
+import { User } from '../entities/users';
+import { ApiRepoUsers } from './api.repo.users';
+
+describe('Given ApiRepoClass', () => {
+  describe('When we instantiate it and response is ok', () => {
+    let jsonMock: jest.Mock;
+    beforeEach(() => {
+      jsonMock = jest.fn().mockResolvedValue({});
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: true,
+        json: jsonMock,
+      });
+    });
+
+    const repo = new ApiRepoUsers();
+
+    test('Then the method userLogin should be used', async () => {
+      const result = await repo.userLogin({} as Partial<User>);
+      expect(jsonMock).toHaveBeenCalled();
+      expect(result).toStrictEqual({});
+    });
+
+    test('Then the method userRegister should be used', async () => {
+      const result = await repo.userRegister({} as FormData);
+      expect(jsonMock).toHaveBeenCalled();
+      expect(result).toStrictEqual({});
+    });
+  });
+
+  describe('When we instantiate it and response is not ok', () => {
+    const errorStatus = 404;
+    const errorStatusText = 'Not Found';
+
+    beforeEach(() => {
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: false,
+        status: errorStatus,
+        statusText: errorStatusText,
+      });
+    });
+
+    const repo = new ApiRepoUsers();
+
+    test('Then the method userLogin should throw an error', async () => {
+      await expect(repo.userLogin({} as User)).rejects.toThrow();
+    });
+
+    test('Then the method userLogin should throw an error', async () => {
+      await expect(repo.userRegister({} as FormData)).rejects.toThrow();
+    });
+  });
+});
---
+++ src/repo/api.repo.users.ts
@@ -0,0 +1,32 @@
+import { User } from '../entities/users.js';
+
+export class ApiRepoUsers {
+  apiUrl = 'http://localhost:2800/users';
+
+  async userLogin(registeredUser: Partial<User>): Promise<User> {
+    // Quiero recibir un email y una passord
+    const finalUrl = `${this.apiUrl}/admin/login`;
+    const apiResponse = await fetch(finalUrl, {
+      method: 'PATCH',
+      body: JSON.stringify(registeredUser),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!apiResponse.ok) {
+      throw new Error(apiResponse.status + ' ' + apiResponse.statusText);
+    }
+    return apiResponse.json();
+  }
+
+  async userRegister(userToRegister: FormData): Promise<User> {
+    const finalUrl = `${this.apiUrl}/admin/register`;
+    const apiResponse = await fetch(finalUrl, {
+      method: 'POST',
+      body: userToRegister,
+    });
+    if (!apiResponse.ok)
+      throw new Error(apiResponse.status + ' ' + apiResponse.statusText);
+    return apiResponse.json();
+  }
+}
