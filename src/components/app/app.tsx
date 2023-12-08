23234ac60b0acb61c16620bb2a3198219da429f6
alejandroileal <aleal1202@outlook.com>
2023-12-08 13:01:15 +0100

    Fix code smell

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,4 @@
 import { AppRoutes } from '../app.routes/app.routes';
-// import { Login } from '../login/login.js';
-// import { Register } from '../register/register.js';
 
 export default function App() {
   return <AppRoutes></AppRoutes>;
