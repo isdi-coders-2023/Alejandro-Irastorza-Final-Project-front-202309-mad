e6e17e61c96d2e3b6508b1143aa404374567f90e
alejandroileal <aleal1202@outlook.com>
2023-12-08 10:38:28 +0100

    Add login component with testing

---
+++ src/components/login/login.tsx
@@ -0,0 +1,53 @@
+import { SyntheticEvent, useState } from 'react';
+import { User } from '../../entities/user';
+import { useUsers } from '../../hooks/use.users';
+
+export function Login() {
+  const { doLogin } = useUsers();
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    const formElement = event.target as HTMLFormElement;
+    const formData = new FormData(formElement);
+    const userLoginData: Partial<User> = {
+      email: formData.get('email')?.toString(),
+      password: formData.get('password')?.toString(),
+      // email: (formElement.elements.namedItem('email') as HTMLInputElement)
+      //   .value,
+      // password: (formElement.elements.namedItem('password') as HTMLInputElement)
+      //   .value,
+    };
+    doLogin(userLoginData);
+    console.log(userLoginData);
+  };
+  return (
+    <form aria-label="form" onSubmit={handleSubmit}>
+      <h2>Inicia sesión</h2>
+      <div className="input-container">
+        <div>
+          <p>Correo electrónico</p>
+          <input required type="email" name="email" />
+        </div>
+        <div>
+          <p>Contraseña</p>
+          <input type={showPassword ? 'text' : 'password'} name="password" />
+          <button
+            name="see-button"
+            type="button"
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
+            data-testid="see-button"
+          >
+            See
+          </button>
+        </div>
+      </div>
+      <button name="submit-button" type="submit">
+        Iniciar sesión
+      </button>
+    </form>
+  );
+}
