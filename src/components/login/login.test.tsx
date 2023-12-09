bd1a547fc279d45699cefc928b454543ea582fee
alejandroileal <aleal1202@outlook.com>
2023-12-09 18:02:21 +0100

    Test missed line

--- src/components/login/login.test.tsx
+++ src/components/login/login.test.tsx
@@ -25,9 +25,13 @@ describe('Login Component', () => {
     const fromElement = screen.getByRole('form');
     const inputElements = screen.getAllByRole('textbox');
     const submitButton = screen.getByRole('button', { name: 'Iniciar sesión' });
+    const showPasswordButton = screen.getByRole('button', { name: 'See' });
+
     await userEvent.type(inputElements[0], 'test');
     await userEvent.type(inputElements[1], 'test');
     await userEvent.click(submitButton);
+    await userEvent.click(showPasswordButton);
+
     await fireEvent.submit(fromElement);
     expect(useUsers().doLogin).toHaveBeenCalled();
     expect(fromElement).toBeInTheDocument();
