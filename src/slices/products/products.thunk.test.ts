c3e285b99ac664e28a9b35bd952a886d8ed1ecd2
alejandroileal <aleal1202@outlook.com>
2023-12-11 15:01:33 +0100

    Add loadOneProductThunk test on current file

--- src/slices/products/products.thunk.test.ts
+++ src/slices/products/products.thunk.test.ts
@@ -1,28 +1,40 @@
 import { Product } from '../../entities/product';
 import { ApiRepoProducts } from '../../repo/api.repo.products';
 import { store } from '../../store/store';
-import { loadProductsThunk } from './products.thunk';
+import { loadOneProductThunk, loadProductsThunk } from './products.thunk';
 
 describe('Given loadProductsThunk', () => {
   describe('When we dispatch succesfully', () => {
     const mockedRepo = {
       getAllProducts: jest.fn().mockReturnValue([] as Product[]),
+      getProductById: jest.fn().mockReturnValue({} as Product),
     } as unknown as ApiRepoProducts;
 
     test('Then it should dispatch', async () => {
       await store.dispatch(loadProductsThunk(mockedRepo));
       expect(mockedRepo.getAllProducts).toHaveBeenCalled();
+    });
+
+    test('Then it should dispatch', async () => {
+      await store.dispatch(loadOneProductThunk({ repo: mockedRepo, id: '' }));
+      expect(mockedRepo.getProductById).toHaveBeenCalled();
     });
   });
+});
 
-  describe('When we dispatch unsuccesfully', () => {
-    const mockedRepo = {
-      getAllProducts: jest.fn().mockRejectedValue([] as Product[]),
-    } as unknown as ApiRepoProducts;
+describe('When we dispatch unsuccesfully', () => {
+  const mockedRepo = {
+    getAllProducts: jest.fn().mockRejectedValue([] as Product[]),
+    getProductById: jest.fn().mockRejectedValue({} as Product),
+  } as unknown as ApiRepoProducts;
 
-    test('Then it should dispatch', async () => {
-      await store.dispatch(loadProductsThunk(mockedRepo));
-      expect(mockedRepo.getAllProducts).toHaveBeenCalled();
-    });
+  test('Then it should dispatch', async () => {
+    await store.dispatch(loadProductsThunk(mockedRepo));
+    expect(mockedRepo.getAllProducts).toHaveBeenCalled();
+  });
+
+  test('Then it should dispatch', async () => {
+    await store.dispatch(loadOneProductThunk({ repo: mockedRepo, id: '' }));
+    expect(mockedRepo.getProductById).toHaveBeenCalled();
   });
 });
