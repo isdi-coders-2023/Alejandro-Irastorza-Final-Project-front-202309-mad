bbfa4341051740503a5f8c004f81fbf1f4b100b9
alejandroileal <aleal1202@outlook.com>
2023-12-11 11:02:03 +0100

    Add products to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from '../slices/users/users.slice.js';
+import productsReducer from '../slices/products/products.slice.js';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    products: productsReducer,
   },
 });
 
