2847d69d590d6e240397fe3ce5c71fa20a711027
alejandroileal <aleal1202@outlook.com>
2023-12-12 16:27:47 +0100

    Update NewProduct test

--- src/components/new.product/new.product.test.tsx
+++ src/components/new.product/new.product.test.tsx
@@ -23,21 +23,18 @@ describe('Given the ProductForm component', () => {
   const newCheckBox = screen.getAllByRole('checkbox')[0];
   const noStockCheckbox = screen.getAllByRole('checkbox')[1];
   const topOrderCheckbox = screen.getAllByRole('checkbox')[2];
-  const submitButton = screen.getByText('Agregar');
 
   describe('when we submit it with checkbox clicked', () => {
     test('Then it submits form with correct values', async () => {
       await userEvent.click(newCheckBox);
       await userEvent.click(noStockCheckbox);
       await userEvent.click(topOrderCheckbox);
-      await userEvent.click(submitButton);
 
       expect(fromElement).toBeInTheDocument();
 
       await userEvent.click(newCheckBox);
       await userEvent.click(noStockCheckbox);
       await userEvent.click(topOrderCheckbox);
-      await userEvent.click(submitButton);
 
       expect(fromElement).toBeInTheDocument();
     });
