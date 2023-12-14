a6fffd227e10f75296b349cafd703db24a01caa8
alejandroileal <aleal1202@outlook.com>
2023-12-14 12:26:41 +0100

    Fix tags to be semantic

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,15 @@
+import './header.style.scss';
+
 type Props = {
   title?: string;
 };
 export function Header({ title }: Props) {
   return (
-    <div>
-      <div>
-        <img src="alien-bar-isotipe.svg" alt="" />
+    <header>
+      <div className="header-logo-container">
+        <img className="header-logo" src="/alien-bar-isotipe.png" alt="" />
       </div>
-      {title && <h2>{title}</h2>}
-    </div>
+      {title && <h2 className="header-title">{title}</h2>}
+    </header>
   );
 }
