61e00a549c808462ce0525892ddc73091da282ca
alejandroileal <aleal1202@outlook.com>
2023-12-14 10:18:44 +0100

    Add details component with missed lines all tested

--- src/components/details/details.test.tsx
+++ src/components/details/details.test.tsx
@@ -1,21 +1,54 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import { store } from '../../store/store';
+import { Product } from '../../entities/product';
 import { Details } from './details';
+import { store } from '../../store/store';
 
 describe('Given AdminCard component', () => {
-  render(
-    <Router>
-      <Provider store={store}>
-        <Details></Details>
-      </Provider>
-    </Router>
-  );
-  test('Then it should render', async () => {
-    const goBackButton = screen.getByText('Regresar');
-    await fireEvent.click(goBackButton);
-    expect(goBackButton).toBeInTheDocument();
+  describe('when the component recieve an array', () => {
+    test('Then it should render', async () => {
+      render(
+        <Router>
+          <Provider store={store}>
+            <Details
+              id="123"
+              products={[
+                {
+                  id: '123',
+                  new: true,
+                  noStock: true,
+                  topOrder: true,
+                  name: 'sample',
+                  modelImg: { url: '123' },
+                } as Product,
+              ]}
+            ></Details>
+          </Provider>
+        </Router>
+      );
+
+      const imgElement = screen.getByRole('img');
+      expect(imgElement).toBeInTheDocument();
+    });
+  });
+
+  describe('when the component dont recieve an array', () => {
+    test('then it should display a message', () => {
+      render(
+        <Router>
+          <Provider store={store}>
+            <Details id="123" products={null}></Details>
+          </Provider>
+        </Router>
+      );
+
+      const messageElement = screen.getByText(
+        'There was an error loading the product.'
+      );
+
+      expect(messageElement).toBeInTheDocument();
+    });
   });
 });
--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,16 +1,51 @@
-export function Details() {
+import { Link } from 'react-router-dom';
+// import { RootState } from '../../store/store';
+// import { useSelector } from 'react-redux';
+import './details.style.scss';
+import { Product } from '../../entities/product';
+
+type Params = {
+  products: Product[] | null;
+  id: string;
+};
+export function Details({ products, id }: Params) {
+  // const { id } = useParams();
+
+  // const { products } = useSelector((state: RootState) => state.products);
+
+  if (!products) {
+    return <div>There was an error loading the product.</div>;
+  }
+
+  const product = products!.find((item) => item.id === id);
+
+  console.log('From Params: ', id);
+
   return (
-    <div>
-      <div>
-        <p>Nuevo</p>
-        <p>Agotado</p>
-        <p>Top pedidos</p>
+    <div className="details-container">
+      <div className="details-tags-container">
+        {product!.new && <p className="details-new-tag">NUEVO</p>}
+        {product!.noStock && <p className="details-noStock-tag">AGOTADO</p>}
+        {product!.topOrder && (
+          <p className="details-top-order-tag">TOP PEDIDOS</p>
+        )}
       </div>
-      <h2>Nombre de la bebida</h2>
-      <h3>Precio</h3>
-      <img src="" />
-      <p>Descripcción del producto</p>
-      <button>Regresar</button>
+      <div className="details-headers-container">
+        <h2>{product!.name}</h2>
+        <h3>${product!.price}</h3>
+      </div>
+      <div className="details-image-container">
+        <img src={product!.modelImg.url} />
+      </div>
+      <div className="details-description-container">
+        <p>{product!.description}</p>
+      </div>
+
+      <button>
+        <Link to={'/menu'} style={{ textDecoration: 'none', color: 'inherit' }}>
+          Regresar
+        </Link>
+      </button>
     </div>
   );
 }
