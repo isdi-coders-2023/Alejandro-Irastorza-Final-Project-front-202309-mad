d9f3d9f1004b7e94bc9b87f597c55e8ae06c9a0c
alejandroileal <aleal1202@outlook.com>
2023-12-14 12:25:41 +0100

    Add login and register page into routes

--- src/components/app.routes/app.routes.tsx
+++ src/components/app.routes/app.routes.tsx
@@ -1,26 +1,39 @@
-import { Routes } from 'react-router-dom';
-
+import { Route, Routes } from 'react-router-dom';
+import { LoginPage } from '../../pages/login.page/login.page';
+import { RegisterPage } from '../../pages/register.page/register.page';
+import { Header } from '../header/header';
+import { MenuPage } from '../../pages/menu.page/menu.page';
+import { ErrorPage } from '../../pages/error.page/error.page';
+import { AddProductPage } from '../../pages/add.product.page/add.product.page';
+import { EditProductPage } from '../../pages/edit.product.page/edit.product.page';
+import { AdminPannelMenu } from '../../pages/admin.pannel.menu.page/admin.pannel.menu.page';
+import { DetailsPage } from '../../pages/details.page/details.page';
 export function AppRoutes() {
   return (
     <main>
       <Routes>
-        {/* <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/register" element={<Register></Register>}></Route> */}
+        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+        <Route path="/register" element={<RegisterPage></RegisterPage>}></Route>
+        <Route
+          path="/admin"
+          element={<AdminPannelMenu></AdminPannelMenu>}
+        ></Route>
+        <Route
+          path="/new-product"
+          element={<AddProductPage></AddProductPage>}
+        ></Route>
+        <Route
+          path="admin/edit-product/:id"
+          element={<EditProductPage></EditProductPage>}
+        ></Route>
+        <Route path="/menu" element={<MenuPage></MenuPage>}></Route>
+        <Route
+          path="/menu/details/:id"
+          element={<DetailsPage></DetailsPage>}
+        ></Route>
+        <Route path="/header" element={<Header></Header>}></Route>
+        <Route path="/*" element={<ErrorPage></ErrorPage>}></Route>
       </Routes>
     </main>
   );
 }
-
-//  <Route path="/" element={<MenuPage></MenuPage>}></Route>
-//       <Route path="/menu" element={<MenuPage></MenuPage>}></Route>
-//       <Route path="/admin/login" element={<LoginPage></LoginPage>}></Route>
-//       <Route
-//         path="/admin/register"
-//         element={<RegisterPage></RegisterPage>}
-//       ></Route>
-//       <Route path="/admin/login" element={<LoginPage></LoginPage>}></Route>
-//       <Route
-//         path="/admin/dashboard"
-//         element={<MenuPannelPage></MenuPannelPage>}
-//       ></Route>
-// Routes
