c0eae48b2a10826ddacc678e4db09afb4a76c5f6
alejandroileal <aleal1202@outlook.com>
2023-12-14 12:27:37 +0100

    Add container tag

--- src/pages/menu.page/menu.page.tsx
+++ src/pages/menu.page/menu.page.tsx
@@ -4,7 +4,8 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import { useEffect } from 'react';
 import { useProducts } from '../../hooks/use.products';
-
+import './menu.page.style.scss';
+import { Heading } from '../../components/heading/heading';
 export function MenuPage() {
   const { products, productState } = useSelector(
     (state: RootState) => state.products
@@ -16,23 +17,29 @@ export function MenuPage() {
   }, [productState]);
 
   return (
-    <>
+    <div className="menu-page-container">
       <Header title="Menú"></Header>
-      <MenuSection
-        products={products}
-        category="Litros 1.0"
-        headingDescription="Pídelo con o sin alcohol"
-      ></MenuSection>
-      <MenuSection
-        products={products}
-        category="Litros 2.0"
-        headingDescription="Nueva gama 2.0 con doble alcohol y fórmulas exclusivas"
-      ></MenuSection>
-      <MenuSection
-        products={products}
-        category="Sueritos"
-        headingDescription="Es como un litro, pero pega más...👀"
-      ></MenuSection>
-    </>
+      <div className="body-container">
+        <Heading
+          imagePath="https://static.wixstatic.com/media/1c872a_7d560af2df0148749feeff4df090f10d~mv2.jpg/v1/fill/w_1880,h_576,usm_1.20_1.00_0.01/file.jpg"
+          imgDescription="Sueritos de Azulito"
+        ></Heading>
+        <MenuSection
+          products={products}
+          category="Litros 1.0"
+          headingDescription="Pídelo con o sin alcohol"
+        ></MenuSection>
+        <MenuSection
+          products={products}
+          category="Litros 2.0"
+          headingDescription="Nueva gama 2.0 con doble alcohol y fórmulas exclusivas"
+        ></MenuSection>
+        <MenuSection
+          products={products}
+          category="Sueritos"
+          headingDescription="Es como un litro, pero pega más...👀"
+        ></MenuSection>
+      </div>
+    </div>
   );
 }
