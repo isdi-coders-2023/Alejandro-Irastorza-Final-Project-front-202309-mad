f030b5473adb5e46ae11889b0b83a004e0e68da4
alejandroileal <aleal1202@outlook.com>
2023-12-15 10:37:48 +0100

    Update page and test

--- src/pages/admin.pannel.menu.page/admin.pannel.menu.page.tsx
+++ src/pages/admin.pannel.menu.page/admin.pannel.menu.page.tsx
@@ -4,18 +4,40 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import { useProducts } from '../../hooks/use.products';
 import { useEffect } from 'react';
+import { HeaderAdmin } from '../../components/header.admin/header.admin';
 
 export function AdminPannelMenu() {
   const { products, productFilter, productState } = useSelector(
     (state: RootState) => state.products
   );
-  const { loadAllProducts } = useProducts();
+
+  const { getByCategory, loadAllProducts } = useProducts();
   useEffect(() => {
     loadAllProducts();
   }, [productState]);
+
+  const { loggedUser } = useSelector((state: RootState) => state.users);
+
+  const handleSelect = () => {
+    const selectedItem = document.getElementById('filter') as HTMLSelectElement;
+
+    if (selectedItem!.value === 'Todos') {
+      loadAllProducts();
+    } else {
+      getByCategory(selectedItem!.value);
+    }
+  };
+
   return (
-    <div className="admin-menu-page-container">
-      <AdminMenu products={products} filter={productFilter}></AdminMenu>
-    </div>
+    <>
+      <HeaderAdmin
+        handleSelectFn={handleSelect}
+        user={loggedUser!}
+        title="Menú"
+      ></HeaderAdmin>
+      <div className="admin-menu-page-container">
+        <AdminMenu products={products} filter={productFilter}></AdminMenu>
+      </div>
+    </>
   );
 }
--- src/pages/admin.pannel.menu.page/admin.pannel.menu.test.tsx
+++ src/pages/admin.pannel.menu.page/admin.pannel.menu.test.tsx
@@ -4,19 +4,56 @@ import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import { AdminPannelMenu } from './admin.pannel.menu.page';
+import userEvent from '@testing-library/user-event';
+import { useProducts } from '../../hooks/use.products';
+
+jest.mock('../../hooks/use.products', () => ({
+  useProducts: jest.fn().mockReturnValue({
+    getByCategory: jest.fn(),
+    loadAllProducts: jest.fn(),
+  }),
+}));
 
 describe('Given Header component', () => {
-  render(
-    <Router>
-      <Provider store={store}>
-        <AdminPannelMenu></AdminPannelMenu>
-      </Provider>
-    </Router>
-  );
   describe('when we render it', () => {
+    beforeEach(() => {
+      render(
+        <Router>
+          <Provider store={store}>
+            <AdminPannelMenu></AdminPannelMenu>
+          </Provider>
+        </Router>
+      );
+    });
+
     test('Then it should display a list', async () => {
       const menuList = screen.getByRole('list');
+
       expect(menuList).toBeInTheDocument();
+    });
+
+    test('Then it should load only Sueritos', async () => {
+      const filter = screen.getByRole('combobox');
+
+      expect(filter).toBeInTheDocument();
+
+      const sueritosOption = screen.getByText('Sueritos');
+      await userEvent.selectOptions(filter, sueritosOption);
+
+      expect(sueritosOption).toBeInTheDocument();
+      expect(useProducts().getByCategory).toHaveBeenCalled();
+    });
+
+    test('Then it should load all products ', async () => {
+      const filter = screen.getByRole('combobox');
+
+      expect(filter).toBeInTheDocument();
+
+      const allOption = screen.getByText('Todos');
+      await userEvent.selectOptions(filter, allOption);
+
+      expect(allOption).toBeInTheDocument();
+      expect(useProducts().loadAllProducts).toHaveBeenCalled();
     });
   });
 });
