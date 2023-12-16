38b424b0205d13a9f2b1ae5af49b0c9d27d9cae3
alejandroileal <aleal1202@outlook.com>
2023-12-16 22:46:50 +0100

    Update product page

--- src/pages/add.product.page/add.product.page.tsx
+++ src/pages/add.product.page/add.product.page.tsx
@@ -2,8 +2,15 @@ import { ProductForm } from '../../components/new.product/new.product';
 import { SyntheticEvent } from 'react';
 import { useProducts } from '../../hooks/use.products';
 import './add.product.page.style.scss';
+// import { RootState } from '../../store/store';
+// import { useSelector } from 'react-redux';
+import { User } from '../../entities/user';
+import { HeaderAdmin } from '../../components/header.admin/header.admin';
 export function AddProductPage() {
   const { addNewProduct } = useProducts();
+
+  // const { loggedUser } = useSelector((state: RootState) => state.users);
+
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
 
@@ -31,15 +38,22 @@ export function AddProductPage() {
       formData.set('topOrder', 'false');
     }
 
-    addNewProduct(formData);
+    const temporalUserId = '112233445566778899009988' as User['id'];
+
+    addNewProduct(formData, temporalUserId);
   };
 
   return (
-    <div className="add-product-page-container">
-      <ProductForm
-        title="Nuevo producto"
-        handleSubmit={handleSubmit}
-      ></ProductForm>
+    <div className="add-product-page-container-master">
+      <HeaderAdmin
+        user={{ name: 'Alejandro', profilePic: { url: 'Google.com' } } as User}
+      ></HeaderAdmin>
+      <div className="add-product-page-container">
+        <ProductForm
+          title="Nuevo producto"
+          handleSubmit={handleSubmit}
+        ></ProductForm>
+      </div>
     </div>
   );
 }
