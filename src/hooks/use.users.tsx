cf3e224055919efe21cd133ef44ab1571434295a
alejandroileal <aleal1202@outlook.com>
2023-12-17 17:51:05 +0100

    Add logout function

--- src/hooks/use.users.tsx
+++ src/hooks/use.users.tsx
@@ -3,6 +3,7 @@ import { AppDispatch } from '../store/store';
 import { ApiRepoUsers } from '../repo/api.repo.users';
 import { User } from '../entities/user';
 import { loginThunk, registerThunk } from '../slices/users/users.thunk';
+import { logout } from '../slices/users/users.slice';
 
 export function useUsers() {
   const dispatch = useDispatch<AppDispatch>();
@@ -19,8 +20,13 @@ export function useUsers() {
     dispatch(registerThunk({ repo: usersRepo, registerUser: userToRegister }));
   };
 
+  const doLogout = () => {
+    dispatch(logout());
+  };
+
   return {
     doLogin,
     doRegister,
+    doLogout,
   };
 }
