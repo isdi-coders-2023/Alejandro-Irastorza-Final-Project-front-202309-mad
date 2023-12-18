6b9e9cf879f6c1f875d5ee6fa9c13173cfd741ec
alejandroileal <aleal1202@outlook.com>
2023-12-18 10:47:51 +0100

    Update test

--- src/pages/menu.page/menu.page.test.tsx
+++ src/pages/menu.page/menu.page.test.tsx
@@ -4,21 +4,41 @@ import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import { MenuPage } from './menu.page';
+import { useProducts } from '../../hooks/use.products';
+import userEvent from '@testing-library/user-event';
 
-describe('Given Header component', () => {
-  render(
-    <Router>
-      <Provider store={store}>
-        <MenuPage></MenuPage>
-      </Provider>
-    </Router>
-  );
+jest.mock('../../hooks/use.Products', () => ({
+  useProducts: jest.fn().mockReturnValue({
+    loadAllProducts: jest.fn(),
+    closeProductPopUp: jest.fn(),
+  }),
+}));
+
+describe('Given Menu Page component', () => {
+  beforeEach(() => {
+    render(
+      <Router>
+        <Provider store={store}>
+          <MenuPage></MenuPage>
+        </Provider>
+      </Router>
+    );
+  });
   describe('when we render it without title', () => {
     test('Then it should render an image', async () => {
       const headerTitle = screen.getAllByRole('heading')[1];
       const componentTitle = screen.getAllByRole('heading')[2];
       expect(headerTitle).toBeInTheDocument();
       expect(componentTitle).toBeInTheDocument();
+    });
+  });
+  describe('when we close PopUp', () => {
+    test('then closePopUp function should be called', async () => {
+      const closeElement = screen.getByText('X');
+
+      await userEvent.click(closeElement);
+
+      expect(useProducts().closeProductPopUp).toHaveBeenCalled();
     });
   });
 });
