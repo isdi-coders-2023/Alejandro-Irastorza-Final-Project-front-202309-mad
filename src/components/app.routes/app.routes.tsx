7733172b143430946923eb37eca3d9ceb5d234d0
alejandroileal <aleal1202@outlook.com>
2023-12-19 09:36:33 +0100

    Fix routes to lazy

--- src/components/app.routes/app.routes.tsx
+++ src/components/app.routes/app.routes.tsx
@@ -1,43 +1,56 @@
 import { Route, Routes } from 'react-router-dom';
-import { LoginPage } from '../../pages/login.page/login.page';
-import { RegisterPage } from '../../pages/register.page/register.page';
-import { Header } from '../header/header';
-import { MenuPage } from '../../pages/menu.page/menu.page';
-import { ErrorPage } from '../../pages/error.page/error.page';
-import { AddProductPage } from '../../pages/add.product.page/add.product.page';
-import { EditProductPage } from '../../pages/edit.product.page/edit.product.page';
-import { AdminPannelMenu } from '../../pages/admin.pannel.menu.page/admin.pannel.menu.page';
-import { DetailsPage } from '../../pages/details.page/details.page';
+import { Suspense, lazy } from 'react';
+
+const LoginPage = lazy(() => import('../../pages/login.page/login.page'));
+
+const RegisterPage = lazy(
+  () => import('../../pages/register.page/register.page')
+);
+const MenuPage = lazy(() => import('../../pages/menu.page/menu.page'));
+const ErrorPage = lazy(() => import('../../pages/error.page/error.page'));
+const AddProductPage = lazy(
+  () => import('../../pages/add.product.page/add.product.page')
+);
+const EditProductPage = lazy(
+  () => import('../../pages/edit.product.page/edit.product.page')
+);
+const AdminPannelMenu = lazy(
+  () => import('../../pages/admin.pannel.menu.page/admin.pannel.menu.page')
+);
+const DetailsPage = lazy(() => import('../../pages/details.page/details.page'));
+
 export function AppRoutes() {
   return (
     <main>
-      <Routes>
-        <Route path="/" element={<MenuPage></MenuPage>}></Route>
-        <Route path="/admin/login" element={<LoginPage></LoginPage>}></Route>
-        <Route
-          path="/admin/register"
-          element={<RegisterPage></RegisterPage>}
-        ></Route>
-        <Route
-          path="/admin"
-          element={<AdminPannelMenu></AdminPannelMenu>}
-        ></Route>
-        <Route
-          path="/new-product"
-          element={<AddProductPage></AddProductPage>}
-        ></Route>
-        <Route
-          path="admin/edit-product/:id"
-          element={<EditProductPage></EditProductPage>}
-        ></Route>
-        <Route path="/menu" element={<MenuPage></MenuPage>}></Route>
-        <Route
-          path="/menu/details/:id"
-          element={<DetailsPage></DetailsPage>}
-        ></Route>
-        <Route path="/header" element={<Header></Header>}></Route>
-        <Route path="/*" element={<ErrorPage></ErrorPage>}></Route>
-      </Routes>
+      <Suspense>
+        {' '}
+        <Routes>
+          <Route path="/" element={<MenuPage></MenuPage>}></Route>
+          <Route path="/admin/login" element={<LoginPage></LoginPage>}></Route>
+          <Route
+            path="/admin/register"
+            element={<RegisterPage></RegisterPage>}
+          ></Route>
+          <Route
+            path="/admin"
+            element={<AdminPannelMenu></AdminPannelMenu>}
+          ></Route>
+          <Route
+            path="/new-product"
+            element={<AddProductPage></AddProductPage>}
+          ></Route>
+          <Route
+            path="admin/edit-product/:id"
+            element={<EditProductPage></EditProductPage>}
+          ></Route>
+          <Route path="/menu" element={<MenuPage></MenuPage>}></Route>
+          <Route
+            path="/menu/details/:id"
+            element={<DetailsPage></DetailsPage>}
+          ></Route>
+          <Route path="/*" element={<ErrorPage></ErrorPage>}></Route>
+        </Routes>
+      </Suspense>
     </main>
   );
 }
